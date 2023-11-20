bcea3ac6792acd6bf9c9f23c25b9c5c71c5d56ce
Daniel Apodaca <apodacaduron@gmail.com>
2023-11-20 09:01:37 +0900

    setup mantine components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,22 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import "@mantine/core/styles.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { MantineProvider, createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <ClerkProvider {...pageProps}>
-    <Component {...pageProps} />
-  </ClerkProvider>
+  return (
+    <MantineProvider theme={theme}>
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </MantineProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);

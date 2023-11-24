421404d85f636cea4294ef5d56be6b3ffaf6fd64
Daniel Apodaca <apodacaduron@gmail.com>
2023-11-24 12:58:33 +0900

    sidebar nav updates

--- src/features/workspace/components/ShellLayout.tsx
+++ src/features/workspace/components/ShellLayout.tsx
@@ -1,9 +1,9 @@
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
-import { AppShell, Group, NavLink, ScrollArea, Text } from "@mantine/core";
+import { AppShell, NavLink, ScrollArea } from "@mantine/core";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import type { ReactNode } from "react";
 import { SIDEBAR_ROUTES } from "~/features/data/routes";
+import { ShellOrganizationSwitcher, ShellUserButton } from ".";
 
 type Props = {
   children: ReactNode;
@@ -13,37 +13,28 @@ export default function ShellLayout(props: Props) {
   const route = useRouter();
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: "sm" }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Group h="100%" px="md" justify="space-between">
-          <Text fw="bold" size="xl" style={{ userSelect: "none" }}>
-            DisplayLyft
-          </Text>
-          <UserButton />
-        </Group>
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
+    <AppShell navbar={{ width: 300, breakpoint: "sm" }} padding="md">
+      <AppShell.Navbar>
         <AppShell.Section>
-          <OrganizationSwitcher hidePersonal />
+          <ShellOrganizationSwitcher />
         </AppShell.Section>
-        <AppShell.Section grow my="md" component={ScrollArea}>
+        <AppShell.Section grow my="md" px="lg" component={ScrollArea}>
           {Object.values(SIDEBAR_ROUTES).map((navLink, index) => (
             <NavLink
               key={index}
               label={navLink.label}
-              leftSection={<navLink.icon />}
+              leftSection={<navLink.icon size="1.8rem" stroke="1.5" />}
               active={route.pathname === navLink.pathname}
               style={{ borderRadius: "8px" }}
               component={Link}
               href={navLink.path(route.query.workspaceId?.toString() ?? "")}
+              styles={{ label: { fontSize: "16px", fontWeight: "500" } }}
             />
           ))}
         </AppShell.Section>
-        <AppShell.Section></AppShell.Section>
+        <AppShell.Section>
+          <ShellUserButton />
+        </AppShell.Section>
       </AppShell.Navbar>
       <AppShell.Main>{props.children}</AppShell.Main>
     </AppShell>
